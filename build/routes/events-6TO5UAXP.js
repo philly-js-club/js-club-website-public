1c7ae773420bc080af7bb138341063559d51675c
tjwds <tjwds@users.noreply.github.com>
2023-06-08 21:45:53 +0000

    deploy: philly-js-club/philly-js-club-website@5d738f70d6fe112babe8c554454679501bf5d609

--- build/routes/events-6TO5UAXP.js
+++ build/routes/events-6TO5UAXP.js
@@ -55,7 +55,7 @@ function CalendarSubscriptionLinks({
 }) {
   const [showMore, setShowMore] = (0, import_react.useState)(false);
   return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "calendar-subscription", children: [
-    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("button", { onClick: () => setShowMore((s) => !s), children: "\u{1F4C6} Subscribe with your favorite calendar client" }, void 0, false, {
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("button", { onClick: (e) => console.log(e) || setShowMore((s) => !s), children: "\u{1F4C6} Subscribe with your favorite calendar client" }, void 0, false, {
       fileName: "app/components/CalendarSubscriptionLinks.tsx",
       lineNumber: 14,
       columnNumber: 4
@@ -181,4 +181,4 @@ export {
   Events as default,
   meta
 };
-//# sourceMappingURL=/build/routes/events-EOJDIM4C.js.map
+//# sourceMappingURL=/build/routes/events-6TO5UAXP.js.map
