da50bfc1a0a15fa0ef92da5ac6bb8b60c3d3a6a8
tjwds <tjwds@users.noreply.github.com>
2023-09-20 22:26:02 +0000

    deploy: philly-js-club/philly-js-club-website@4d3d1522c11d78da43e58da28291b508de8bc69c

--- build/_shared/chunk-C2E3ZVGS.js
+++ build/_shared/chunk-C2E3ZVGS.js
@@ -8,7 +8,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/config.ts"
   );
-  import.meta.hot.lastModified = "1695244757595.9946";
+  import.meta.hot.lastModified = "1695248698382.49";
 }
 var site = {
   title: "Philly JS Club",
@@ -26,4 +26,4 @@ export {
   site,
   region
 };
-//# sourceMappingURL=/build/_shared/chunk-CK2VP6ZT.js.map
+//# sourceMappingURL=/build/_shared/chunk-C2E3ZVGS.js.map
