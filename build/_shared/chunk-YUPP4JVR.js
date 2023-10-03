a66018be5e552ac18f26ddbc54b66b3f5d104ba9
tjwds <tjwds@users.noreply.github.com>
2023-10-03 11:19:53 +0000

    deploy: philly-js-club/philly-js-club-website@02720d9e81b4bb7767b8e05e3d49646cc8a86721

--- build/_shared/chunk-YUPP4JVR.js
+++ build/_shared/chunk-YUPP4JVR.js
@@ -8,7 +8,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/config.ts"
   );
-  import.meta.hot.lastModified = "1696328906345.6536";
+  import.meta.hot.lastModified = "1696331940937.5588";
 }
 var site = {
   title: "Philly JS Club",
@@ -26,4 +26,4 @@ export {
   site,
   region
 };
-//# sourceMappingURL=/build/_shared/chunk-WQSOHZTZ.js.map
+//# sourceMappingURL=/build/_shared/chunk-YUPP4JVR.js.map
--- build/routes/about-EMX6YOJQ.js
+++ build/routes/about-EMX6YOJQ.js
@@ -1,10 +1,10 @@
 import {
   PageGrid
-} from "/build/_shared/chunk-A3PYHGCQ.js";
+} from "/build/_shared/chunk-ALS4SWKQ.js";
 import {
   constructSiteTitle
-} from "/build/_shared/chunk-VLQOANOK.js";
-import "/build/_shared/chunk-WQSOHZTZ.js";
+} from "/build/_shared/chunk-ZKDWFDZH.js";
+import "/build/_shared/chunk-YUPP4JVR.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -40,7 +40,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/about.tsx"
   );
-  import.meta.hot.lastModified = "1696328906345.6536";
+  import.meta.hot.lastModified = "1696331940941.5588";
 }
 var meta = () => {
   return [{
@@ -161,4 +161,4 @@ export {
   About as default,
   meta
 };
-//# sourceMappingURL=/build/routes/about-NHXYLC3M.js.map
+//# sourceMappingURL=/build/routes/about-EMX6YOJQ.js.map
