6d38e3453c13c54aa267d9abb3872efb860223d2
tjwds <tjwds@users.noreply.github.com>
2023-10-08 12:38:16 +0000

    deploy: philly-js-club/philly-js-club-website@02720d9e81b4bb7767b8e05e3d49646cc8a86721

--- build/_shared/chunk-A37X3FY6.js
+++ build/_shared/chunk-A37X3FY6.js
@@ -1,6 +1,6 @@
 import {
   site
-} from "/build/_shared/chunk-3J4SER42.js";
+} from "/build/_shared/chunk-BGTVHTHB.js";
 import {
   createHotContext
 } from "/build/_shared/chunk-4RRP2SUU.js";
@@ -11,7 +11,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/utils/common.ts"
   );
-  import.meta.hot.lastModified = "1696763900145.6443";
+  import.meta.hot.lastModified = "1696768633275.8477";
 }
 function constructSiteTitle(pageTitle) {
   if (!pageTitle)
@@ -26,4 +26,4 @@ export {
   constructSiteTitle,
   upperFirst
 };
-//# sourceMappingURL=/build/_shared/chunk-YMKWQ76R.js.map
+//# sourceMappingURL=/build/_shared/chunk-A37X3FY6.js.map
--- build/routes/events-XXZ2LH2D.js
+++ build/routes/events-XXZ2LH2D.js
@@ -1,16 +1,16 @@
 import {
   EventDetails,
   require_node
-} from "/build/_shared/chunk-ZJQI6EV6.js";
+} from "/build/_shared/chunk-VZRWOPOS.js";
 import {
   PageGrid
-} from "/build/_shared/chunk-YBNAX6HL.js";
+} from "/build/_shared/chunk-ZWL37GO2.js";
 import {
   constructSiteTitle
-} from "/build/_shared/chunk-YMKWQ76R.js";
+} from "/build/_shared/chunk-A37X3FY6.js";
 import {
   site
-} from "/build/_shared/chunk-3J4SER42.js";
+} from "/build/_shared/chunk-BGTVHTHB.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -52,7 +52,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/components/CalendarSubscriptionLinks.tsx"
   );
-  import.meta.hot.lastModified = "1696763900141.6443";
+  import.meta.hot.lastModified = "1696768633271.8477";
 }
 function CalendarSubscriptionLinks({
   webcalUrl
@@ -128,7 +128,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/events.tsx"
   );
-  import.meta.hot.lastModified = "1696763900141.6443";
+  import.meta.hot.lastModified = "1696768633271.8477";
 }
 var meta = () => {
   return [{
@@ -190,4 +190,4 @@ export {
   Events as default,
   meta
 };
-//# sourceMappingURL=/build/routes/events-ZLQALEGY.js.map
+//# sourceMappingURL=/build/routes/events-XXZ2LH2D.js.map
