ebe4b804bf5d8f0c9baad9e321a5651caecec56f
tjwds <tjwds@users.noreply.github.com>
2023-10-24 10:29:19 +0000

    deploy: philly-js-club/philly-js-club-website@ef82ed6fda15ec9a665c39f41d53ff42208bbd25

--- build/routes/_index-LSMNWNRK.js
+++ build/routes/_index-LSMNWNRK.js
@@ -1,14 +1,14 @@
 import {
   EventDetails,
   require_node
-} from "/build/_shared/chunk-64JHQRZG.js";
+} from "/build/_shared/chunk-3OABHRHW.js";
 import {
   PageGrid
-} from "/build/_shared/chunk-ESWPFE26.js";
+} from "/build/_shared/chunk-MT3NM3OJ.js";
 import {
   constructSiteTitle
-} from "/build/_shared/chunk-7P7QWAPY.js";
-import "/build/_shared/chunk-VWI7PXTC.js";
+} from "/build/_shared/chunk-CNO64SM4.js";
+import "/build/_shared/chunk-X4MBUFPZ.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -45,7 +45,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/_index.tsx"
   );
-  import.meta.hot.lastModified = "1698139375271.5957";
+  import.meta.hot.lastModified = "1698143301639.3115";
 }
 var meta = () => {
   return [{
@@ -103,4 +103,4 @@ export {
   Index as default,
   meta
 };
-//# sourceMappingURL=/build/routes/_index-IYNG6ACI.js.map
+//# sourceMappingURL=/build/routes/_index-LSMNWNRK.js.map
