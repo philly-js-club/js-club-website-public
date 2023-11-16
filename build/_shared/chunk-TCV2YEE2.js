f390eba5b2162b1f0585e4b2249a9bbc30e40135
tjwds <tjwds@users.noreply.github.com>
2023-11-16 16:32:21 +0000

    deploy: philly-js-club/philly-js-club-website@2e64f25200f2b84c7e3f03452d2d1cadcc322ab7

---
+++ build/_shared/chunk-TCV2YEE2.js
@@ -0,0 +1,29 @@
+import {
+  site
+} from "/build/_shared/chunk-3MX74ZGM.js";
+import {
+  createHotContext
+} from "/build/_shared/chunk-4RRP2SUU.js";
+
+// app/utils/common.ts
+if (import.meta) {
+  import.meta.hot = createHotContext(
+    //@ts-expect-error
+    "app/utils/common.ts"
+  );
+  import.meta.hot.lastModified = "1700152290116.913";
+}
+function constructSiteTitle(pageTitle) {
+  if (!pageTitle)
+    return site.title;
+  return `${pageTitle} | ${site.title}`;
+}
+function upperFirst(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
+
+export {
+  constructSiteTitle,
+  upperFirst
+};
+//# sourceMappingURL=/build/_shared/chunk-TCV2YEE2.js.map
