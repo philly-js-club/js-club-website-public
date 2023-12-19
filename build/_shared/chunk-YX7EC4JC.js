fac42ae5fff12f8deb73788277193b0ead5fff72
tjwds <tjwds@users.noreply.github.com>
2023-12-19 10:29:56 +0000

    deploy: philly-js-club/philly-js-club-website@2e64f25200f2b84c7e3f03452d2d1cadcc322ab7

--- build/_shared/chunk-YX7EC4JC.js
+++ build/_shared/chunk-YX7EC4JC.js
@@ -1,9 +1,9 @@
 import {
   Arrow
-} from "/build/_shared/chunk-4U2SZ2CT.js";
+} from "/build/_shared/chunk-KW4KPZ6S.js";
 import {
   region
-} from "/build/_shared/chunk-I6MRCWVO.js";
+} from "/build/_shared/chunk-GOERJXKK.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -41,7 +41,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/components/EventDetails.tsx"
   );
-  import.meta.hot.lastModified = "1702977768284.223";
+  import.meta.hot.lastModified = "1702981744436.621";
 }
 function EventDetails({
   date,
@@ -104,4 +104,4 @@ export {
   require_node,
   EventDetails
 };
-//# sourceMappingURL=/build/_shared/chunk-MJKVNOMM.js.map
+//# sourceMappingURL=/build/_shared/chunk-YX7EC4JC.js.map
