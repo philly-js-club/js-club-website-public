78dcb20adc974eeb6c664006c00f7a32980711ff
tjwds <tjwds@users.noreply.github.com>
2023-12-21 13:29:34 +0000

    deploy: philly-js-club/philly-js-club-website@2e64f25200f2b84c7e3f03452d2d1cadcc322ab7

--- build/routes/about-XXLBM64U.js
+++ build/routes/about-XXLBM64U.js
@@ -1,10 +1,10 @@
 import {
   PageGrid
-} from "/build/_shared/chunk-BO7H7F2U.js";
+} from "/build/_shared/chunk-SBC7QYW4.js";
 import {
   constructSiteTitle
-} from "/build/_shared/chunk-MHG4T72B.js";
-import "/build/_shared/chunk-RJCYVEGC.js";
+} from "/build/_shared/chunk-HHFUZOXP.js";
+import "/build/_shared/chunk-5FHYO5TL.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -41,7 +41,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/about.tsx"
   );
-  import.meta.hot.lastModified = "1703162415639.0754";
+  import.meta.hot.lastModified = "1703165320512.8389";
 }
 var meta = () => {
   return [{
@@ -162,4 +162,4 @@ export {
   About as default,
   meta
 };
-//# sourceMappingURL=/build/routes/about-6BEQKRAY.js.map
+//# sourceMappingURL=/build/routes/about-XXLBM64U.js.map
