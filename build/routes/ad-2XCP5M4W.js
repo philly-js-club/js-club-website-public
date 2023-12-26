80b0247dca632255e253db5598aad2e10a2b8895
tjwds <tjwds@users.noreply.github.com>
2023-12-26 20:23:34 +0000

    deploy: philly-js-club/philly-js-club-website@2e64f25200f2b84c7e3f03452d2d1cadcc322ab7

--- build/routes/ad-2XCP5M4W.js
+++ build/routes/ad-2XCP5M4W.js
@@ -1,10 +1,10 @@
 import {
   constructSiteTitle,
   upperFirst
-} from "/build/_shared/chunk-YCTD4EHE.js";
+} from "/build/_shared/chunk-YOXDXD2U.js";
 import {
   site
-} from "/build/_shared/chunk-2EBBVHQI.js";
+} from "/build/_shared/chunk-QCWA6GU3.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -40,7 +40,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/components/AdLogo.tsx"
   );
-  import.meta.hot.lastModified = "1703618282379.7153";
+  import.meta.hot.lastModified = "1703622157582.3494";
 }
 function AdLogo({
   className
@@ -102,7 +102,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/ad.tsx"
   );
-  import.meta.hot.lastModified = "1703618282379.7153";
+  import.meta.hot.lastModified = "1703622157586.3494";
 }
 var meta = ({
   location
@@ -199,4 +199,4 @@ export {
   Ad as default,
   meta
 };
-//# sourceMappingURL=/build/routes/ad-MPKZZO46.js.map
+//# sourceMappingURL=/build/routes/ad-2XCP5M4W.js.map
--- build/routes/sponsors-MLWKQ7MV.js
+++ build/routes/sponsors-MLWKQ7MV.js
@@ -1,13 +1,13 @@
 import {
   Icons
-} from "/build/_shared/chunk-5UWOBPQM.js";
+} from "/build/_shared/chunk-RGLYHJFC.js";
 import {
   PageGrid
-} from "/build/_shared/chunk-767WRHNV.js";
+} from "/build/_shared/chunk-MJMEOXW4.js";
 import {
   constructSiteTitle
-} from "/build/_shared/chunk-YCTD4EHE.js";
-import "/build/_shared/chunk-2EBBVHQI.js";
+} from "/build/_shared/chunk-YOXDXD2U.js";
+import "/build/_shared/chunk-QCWA6GU3.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -44,7 +44,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/sponsors.tsx"
   );
-  import.meta.hot.lastModified = "1703618282379.7153";
+  import.meta.hot.lastModified = "1703622157586.3494";
 }
 var meta = () => {
   return [{
@@ -100,4 +100,4 @@ export {
   Sponsors as default,
   meta
 };
-//# sourceMappingURL=/build/routes/sponsors-QWV36YGQ.js.map
+//# sourceMappingURL=/build/routes/sponsors-MLWKQ7MV.js.map
