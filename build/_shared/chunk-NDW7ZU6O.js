c1a67f7aa4cfca586b825b8ba4b85513abe9e9d5
tjwds <tjwds@users.noreply.github.com>
2024-01-10 19:19:07 +0000

    deploy: philly-js-club/philly-js-club-website@186137c68332c3acb7d50c5115bfe2787f07fbd8

--- build/_shared/chunk-NDW7ZU6O.js
+++ build/_shared/chunk-NDW7ZU6O.js
@@ -8,7 +8,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/config.ts"
   );
-  import.meta.hot.lastModified = "1704911268173.3828";
+  import.meta.hot.lastModified = "1704914296566.0159";
 }
 var site = {
   title: "Philly JS Club",
@@ -26,4 +26,4 @@ export {
   site,
   region
 };
-//# sourceMappingURL=/build/_shared/chunk-YSFZGBMG.js.map
+//# sourceMappingURL=/build/_shared/chunk-NDW7ZU6O.js.map
--- build/routes/events-VBA5GZV5.js
+++ build/routes/events-VBA5GZV5.js
@@ -1,16 +1,16 @@
 import {
   EventDetails,
   require_node
-} from "/build/_shared/chunk-3CBTZJK5.js";
+} from "/build/_shared/chunk-L62HYS3G.js";
 import {
   PageGrid
-} from "/build/_shared/chunk-LOXRU2O3.js";
+} from "/build/_shared/chunk-L7BH5POR.js";
 import {
   constructSiteTitle
-} from "/build/_shared/chunk-HO4OP4XV.js";
+} from "/build/_shared/chunk-WEN5USV4.js";
 import {
   site
-} from "/build/_shared/chunk-YSFZGBMG.js";
+} from "/build/_shared/chunk-NDW7ZU6O.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -53,7 +53,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/components/CalendarSubscriptionLinks.tsx"
   );
-  import.meta.hot.lastModified = "1704911268173.3828";
+  import.meta.hot.lastModified = "1704914296566.0159";
 }
 function CalendarSubscriptionLinks({
   webcalUrl
@@ -129,7 +129,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/events.tsx"
   );
-  import.meta.hot.lastModified = "1704911268173.3828";
+  import.meta.hot.lastModified = "1704914296566.0159";
 }
 var meta = () => {
   return [{
@@ -191,4 +191,4 @@ export {
   Events as default,
   meta
 };
-//# sourceMappingURL=/build/routes/events-2HQMP3TG.js.map
+//# sourceMappingURL=/build/routes/events-VBA5GZV5.js.map
