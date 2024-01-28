c1d7722dbc0491cade95e923fc94d1b087e486b0
tjwds <tjwds@users.noreply.github.com>
2024-01-28 16:26:06 +0000

    deploy: philly-js-club/philly-js-club-website@5b3f532658e27c3b6435eed0ab7fdcd80ca8badb

--- build/routes/events-IVV44EHM.js
+++ build/routes/events-IVV44EHM.js
@@ -1,16 +1,16 @@
 import {
   EventDetails,
   require_node
-} from "/build/_shared/chunk-L2ZQ7LKA.js";
+} from "/build/_shared/chunk-NSSBWA54.js";
 import {
   PageGrid
-} from "/build/_shared/chunk-CSVZ6ZZ5.js";
+} from "/build/_shared/chunk-MNUR2JL2.js";
 import {
   constructSiteTitle
-} from "/build/_shared/chunk-H65KNWQQ.js";
+} from "/build/_shared/chunk-LXCPBOAV.js";
 import {
   site
-} from "/build/_shared/chunk-A4UGKBKD.js";
+} from "/build/_shared/chunk-ZBSK4FRW.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -53,7 +53,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/components/CalendarSubscriptionLinks.tsx"
   );
-  import.meta.hot.lastModified = "1706455155530.9824";
+  import.meta.hot.lastModified = "1706459101910.2202";
 }
 function CalendarSubscriptionLinks({
   webcalUrl
@@ -129,7 +129,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/routes/events.tsx"
   );
-  import.meta.hot.lastModified = "1706455155530.9824";
+  import.meta.hot.lastModified = "1706459101910.2202";
 }
 var meta = () => {
   return [{
@@ -191,4 +191,4 @@ export {
   Events as default,
   meta
 };
-//# sourceMappingURL=/build/routes/events-FTDDPJRY.js.map
+//# sourceMappingURL=/build/routes/events-IVV44EHM.js.map
