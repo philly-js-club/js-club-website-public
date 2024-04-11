0ea608d4dd1ec14fa966baf3e360aacee3cc4062
tjwds <tjwds@users.noreply.github.com>
2024-04-11 14:26:04 +0000

    deploy: philly-js-club/philly-js-club-website@6533ea39553c3d470ec6ac8d17b2ba9a9de7d1ea

--- build/_shared/chunk-6BI35ZJX.js
+++ build/_shared/chunk-6BI35ZJX.js
@@ -8,7 +8,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/config.ts"
   );
-  import.meta.hot.lastModified = "1712841810937.726";
+  import.meta.hot.lastModified = "1712845513639.361";
 }
 var site = {
   title: "Philly JS Club",
@@ -26,4 +26,4 @@ export {
   site,
   region
 };
-//# sourceMappingURL=/build/_shared/chunk-D5HECIGQ.js.map
+//# sourceMappingURL=/build/_shared/chunk-6BI35ZJX.js.map
--- build/_shared/chunk-YPV2KNKJ.js
+++ build/_shared/chunk-YPV2KNKJ.js
@@ -1,9 +1,9 @@
 import {
   Arrow
-} from "/build/_shared/chunk-YBWVTCEX.js";
+} from "/build/_shared/chunk-CQ6BQ6SG.js";
 import {
   region
-} from "/build/_shared/chunk-D5HECIGQ.js";
+} from "/build/_shared/chunk-6BI35ZJX.js";
 import {
   require_jsx_dev_runtime
 } from "/build/_shared/chunk-XU7DNSPJ.js";
@@ -41,7 +41,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/components/EventDetails.tsx"
   );
-  import.meta.hot.lastModified = "1712841810937.726";
+  import.meta.hot.lastModified = "1712845513639.361";
 }
 function EventDetails({
   date,
@@ -104,4 +104,4 @@ export {
   require_node,
   EventDetails
 };
-//# sourceMappingURL=/build/_shared/chunk-6VEXOVCP.js.map
+//# sourceMappingURL=/build/_shared/chunk-YPV2KNKJ.js.map
