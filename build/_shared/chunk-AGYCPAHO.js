31c4e149a1412f641c91fa6213d6618fb6373b4d
tjwds <tjwds@users.noreply.github.com>
2024-06-03 01:37:47 +0000

    deploy: philly-js-club/philly-js-club-website@fa88509a5d0441ede043bb48c0a19a79b89533f4

--- build/_shared/chunk-AGYCPAHO.js
+++ build/_shared/chunk-AGYCPAHO.js
@@ -8,7 +8,7 @@ if (import.meta) {
     //@ts-expect-error
     "app/config.ts"
   );
-  import.meta.hot.lastModified = "1717370597702.5862";
+  import.meta.hot.lastModified = "1717378616433.7808";
 }
 var site = {
   title: "Philly JS Club",
@@ -26,4 +26,4 @@ export {
   site,
   region
 };
-//# sourceMappingURL=/build/_shared/chunk-MTCTVKRL.js.map
+//# sourceMappingURL=/build/_shared/chunk-AGYCPAHO.js.map
