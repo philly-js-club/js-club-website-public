965b8c26ce2e73ad94ecc08f79ffa680b4f451d9
tjwds <tjwds@users.noreply.github.com>
2024-08-14 22:28:22 +0000

    deploy: philly-js-club/philly-js-club-website@4647b84110889affdaf8cfe5994dce30ee09fa46

--- app/routes/sponsors.tsx
+++ app/routes/sponsors.tsx
@@ -1,6 +1,6 @@
 import { createHotContext as __vite__createHotContext } from "/@vite/client";import.meta.hot = __vite__createHotContext("/app/routes/sponsors.tsx");import RefreshRuntime from "/@id/__x00__virtual:remix/hmr-runtime";const inWebWorker = typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope;let prevRefreshReg;let prevRefreshSig;if (import.meta.hot && !inWebWorker) {  if (!window.__vite_plugin_react_preamble_installed__) {    throw new Error(      "Remix Vite plugin can't detect preamble. Something is wrong."    );  }  prevRefreshReg = window.$RefreshReg$;  prevRefreshSig = window.$RefreshSig$;  window.$RefreshReg$ = (type, id) => {    RefreshRuntime.register(type, "/home/runner/work/philly-js-club-website/philly-js-club-website/app/routes/sponsors.tsx" + " " + id)  };  window.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;}var _s = $RefreshSig$();
-import __vite__cjsImport2_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=06683829"; const Fragment = __vite__cjsImport2_react_jsxDevRuntime["Fragment"]; const jsxDEV = __vite__cjsImport2_react_jsxDevRuntime["jsxDEV"];
-import { useLoaderData } from "/node_modules/.vite/deps/@remix-run_react.js?v=06683829";
+import __vite__cjsImport2_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=ed2741e6"; const Fragment = __vite__cjsImport2_react_jsxDevRuntime["Fragment"]; const jsxDEV = __vite__cjsImport2_react_jsxDevRuntime["jsxDEV"];
+import { useLoaderData } from "/node_modules/.vite/deps/@remix-run_react.js?v=ed2741e6";
 import { Icons } from "/app/components/Icons.tsx";
 import { PageGrid } from "/app/components/PageGrid.tsx";
 import { constructSiteTitle } from "/app/utils/common.ts";
